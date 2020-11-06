dedc598b314ede9044c15ad9dd6f42d25a8dfe48
Thao Thi Ngoc Truong <ttngocthao_87@yahoo.com>
2020-11-06 00:50:05 +0000

    Clean up ignored files

--- src/firebase.config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyAZtG3MhJi60pOON0EGCULAnYyG9fyQd0M",
-  authDomain: "manage-amount.firebaseapp.com",
-  databaseURL: "https://manage-amount.firebaseio.com",
-  projectId: "manage-amount",
-  storageBucket: "manage-amount.appspot.com",
-  messagingSenderId: "439368605018",
-  appId: "1:439368605018:web:a63d2d90e1f9a71f706763",
-};
